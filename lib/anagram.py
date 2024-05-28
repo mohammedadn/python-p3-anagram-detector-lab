4eed297ed82b039fa8d80d1ef8db962b90163ee6
Mohammedadn <mohammedaden023@gmail.com>
2024-05-28 10:17:29 +0300

    test

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+# your code goes here!
+class Anagram():
+    
+    def __init__(self, word):
+        self.words = sorted([letter for letter in word]) 
+    
+    def match(self,  arr):        
+        matched = []
+        for i in arr:
+            if sorted([letter for letter in i]) == self.words:
+                matched.append(i)
+
+        return matched
+   
+    
